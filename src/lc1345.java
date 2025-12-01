e7a4be34273103abb9415b85c087cb72f761d514
Duy <duy.leduc8@gmail.com>
2025-12-01 10:28:52 +0700

    20251201

---
+++ src/lc1345.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.util.Set;
+
+public class lc1345 {
+    static final Set<Character> VOWELS = Set.of('a', 'u', 'i', 'e', 'o', 'A', 'U', 'I', 'E', 'O');
+    
+    public static void main(String[] args) {
+        System.out.println(reverseVowels("IceCreAm")); // AceCreIm
+        System.out.println(reverseVowels("leetcode")); // leotcede
+    }
+    
+    public static String reverseVowels(String s) {
+        char[] res = s.toCharArray();
+        int left = 0;
+        int right = res.length - 1;
+        
+        while (left < right) {
+            while (left < right && !isVowel(res[left])) left++;
+            while (left < right && !isVowel(res[right])) right--;
+            
+            char temp = res[left];
+            res[left] = res[right];
+            res[right] = temp;
+            
+            left++;
+            right--;
+        }
+        
+        return new String(res);
+    }
+    
+    public static boolean isVowel(char c) {
+        return VOWELS.contains(c);
+    }
+}
