c4bea8a584a3d76a2b7f816ad77685c7f13590ae
Duy <duy.leduc8@gmail.com>
2026-02-23 16:29:33 +0700

    20260223

---
+++ src/lc235.java
@@ -0,0 +1,33 @@
+package src;
+
+public class lc235 {
+    public static void main(String[] args) {
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Need to understand BST definition to solve this
+        while (root != null) {
+            if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
