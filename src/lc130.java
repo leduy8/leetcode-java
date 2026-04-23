0eb9cbba914f95007d7a2d41813988a4bcccf048
Duy <duy.leduc8@gmail.com>
2026-04-23 14:05:22 +0700

    20260423

--- src/lc130.java
+++ src/lc130.java
@@ -2,7 +2,11 @@ package src;
 
 public class lc130 {
     public static void main(String[] args) {
-        solve(new char[][] {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}});
+        solve(new char[][] {{'X','X','X','X'},
+                            {'X','O','O','X'},
+                            {'X','X','O','X'},
+                            {'X','O','X','X'}});
+
         solve(new char[][] {{'X'}});
     }
 
@@ -13,6 +17,7 @@ public class lc130 {
         int rows = board.length;
         int cols = board[0].length;
 
+        // Traverse from border inside (Every 'O' we found is automatically "safe" and marked them as "#")
         for (int i = 0; i < rows; i++) {
             if (board[i][0] == 'O') dfs(board, i, 0);
             if (board[i][cols - 1] == 'O') dfs(board, i, cols - 1);
@@ -26,10 +31,12 @@ public class lc130 {
         System.out.println("After marked");
         printBoard(board);
 
+        // Flip O with X (Trapped O)
+        // Flip # with O (Safe 0)
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (board[i][j] == 'O') board[i][j] = 'X'; // Trapped
-                else if (board[i][j] == '#') board[i][j] = 'O'; // Not surrounded
+                else if (board[i][j] == '#') board[i][j] = 'O'; // Safe
             }
         }
 
@@ -44,7 +51,7 @@ public class lc130 {
         // Boundary and 'O' check
         if (r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] != 'O') return;
         
-        // Mark visited/not surrounded
+        // Mark visited/safe
         board[r][c] = '#';
         
         // Explore neighbors
