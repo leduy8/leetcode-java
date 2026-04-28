d3512b380c917cfa90ade82d0f796ea982d2ef94
Duy <duy.leduc8@gmail.com>
2026-04-28 17:19:01 +0700

    20260428

---
+++ src/lc53.java
@@ -0,0 +1,23 @@
+package src;
+
+public class lc53 {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4})); // 6
+        System.out.println(maxSubArray(new int[] {1})); // 1
+        System.out.println(maxSubArray(new int[] {5,4,-1,7,8})); // 23
+        System.out.println(maxSubArray(new int[] {1,2,3,4,-100,101})); // 101
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Decision tree: Expand subarray or start-over?
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
