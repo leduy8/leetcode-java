b63982143853a86fb3c9eba745471593d0237914
Duy <duy.leduc8@gmail.com>
2026-06-12 08:37:09 +0700

    20260612

--- src/school/programmers/co/kr/course30/lesson176962.java
+++ src/school/programmers/co/kr/course30/lesson176962.java
@@ -4,30 +4,70 @@ import java.util.*;
 
 public class lesson176962 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new String[][]{{"korean", "11:40", "30"}, {"english", "12:10", "20"}, {"math", "12:30", "40"}}))); // ["korean", "english", "math"]
-        System.out.println(Arrays.toString(solution(new String[][]{{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}}))); // ["science", "history", "computer", "music"]
-        System.out.println(Arrays.toString(solution(new String[][]{{"aaa", "12:00", "20"}, {"bbb", "12:10", "30"}, {"ccc", "12:40", "10"}}))); // ["bbb", "ccc", "aaa"]
+        System.out.println(Arrays.toString(solution(new String[][]{
+                {"korean", "11:40", "30"}, 
+                {"english", "12:10", "20"}, 
+                {"math", "12:30", "40"}}))); // ["korean", "english", "math"]
+        System.out.println(Arrays.toString(solution(new String[][]{
+                {"science", "12:40", "50"}, 
+                {"music", "12:20", "40"}, 
+                {"history", "14:00", "30"}, 
+                {"computer", "12:30", "100"}}))); // ["science", "history", "computer", "music"]
+        System.out.println(Arrays.toString(solution(new String[][]{
+                {"aaa", "12:00", "20"}, 
+                {"bbb", "12:10", "30"}, 
+                {"ccc", "12:40", "10"}}))); // ["bbb", "ccc", "aaa"]
     }
 
     public static String[] solution(String[][] plans) {
         List<String> answer = new ArrayList<>();
-        Deque<String[]> stack = new ArrayDeque();
-        String curPlan = null;
-        String curTime = null;
-        String curRemainingTime = null;
+        Deque<String[]> stack = new ArrayDeque<>();
 
-        for (String[] plan: plans) {
-            // First iteration
-            if (curPlan == null) {
-                curPlan = plan[0];
-                curTime = plan[1];
-                curRemainingTime = plan[2];
+        // Sort by start time
+        Arrays.sort(plans, Comparator.comparing(p -> toMinutes(p[1])));
+
+        String curName = plans[0][0];
+        int curTime = toMinutes(plans[0][1]);
+        int curRemain = Integer.parseInt(plans[0][2]);
+
+        for (int i = 1; i < plans.length; i++) {
+            int nextStart = toMinutes(plans[i][1]);
+            int availableTime = nextStart - curTime;
+            
+            // Try to finish current plan + stack plans
+            while (true) {
+                if (curRemain <= availableTime) {
+                    // Finish current plan
+                    availableTime -= curRemain;
+                    answer.add(curName);
+                    
+                    if (stack.isEmpty()) {
+                        break;
+                    }
+                    
+                    // Resume paused plan
+                    String[] prev = stack.pop();
+                    curName = prev[0];
+                    curRemain = Integer.parseInt(prev[1]);
+                } else {
+                    // Can't finish -> pause current
+                    curRemain -= availableTime;
+                    stack.push(new String[] {curName, String.valueOf(curRemain)});
+                    break;
+                }
             }
-            // TODO
+            
+            // Start new plan
+            curName = plans[i][0];
+            curTime = nextStart;
+            curRemain = Integer.parseInt(plans[i][2]);
         }
+        
+        // Finish last running plan
+        answer.add(curName);
 
         // Resolve remaining plans
-        while(stack.size() > 0) {
+        while(!stack.isEmpty()) {
             String[] temp = stack.pop();
             answer.add(temp[0]);
         }
